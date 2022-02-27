44296db24f5a010e31ee65ae5ffe35b13d3d3d4d
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-27 22:20:21 +0100

    added welcome

---
+++ components/Welcome.tsx
@@ -0,0 +1,20 @@
+interface WelcomeProps {
+    startFirstRound: () => void;
+}
+
+export default function Welcome({ startFirstRound }: WelcomeProps) {
+    return (
+        <div className="bg-cyan-500 absolute top-0 right-0 bottom-0 left-0 p-12 flex flex-col justify-center items-center gap-12">
+            <p className="text-center text-2xl">
+                Welcome to note training! Guess ten notes in treble and bass
+                clef. After each round you get your average guessing time.
+            </p>
+            <button
+                onClick={startFirstRound}
+                className="bg-white rounded-md p-4"
+            >
+                Start
+            </button>
+        </div>
+    );
+}
