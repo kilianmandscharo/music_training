903bd224dacd52b8579699449caf50c2fa705842
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-27 22:19:54 +0100

    added keypad

--- components/Keypad.tsx
+++ components/Keypad.tsx
@@ -2,13 +2,15 @@ const keys = ["C", "D", "E", "F", "G", "A", "H"];
 
 interface KeypadProps {
     handleInput: (inputNote: string) => void;
+    disabled: boolean;
 }
 
-export default function Keypad({ handleInput }: KeypadProps) {
+export default function Keypad({ handleInput, disabled }: KeypadProps) {
     return (
         <div>
             {keys.map((letter, i) => (
                 <button
+                    disabled={disabled}
                     key={i}
                     onClick={() => handleInput(letter)}
                     className="m-8 bg-orange-300 w-10 rounded-lg"
