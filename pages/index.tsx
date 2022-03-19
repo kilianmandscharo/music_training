8d1fc47c97a29c4b4f585a932c701dd01f1dbe8b
Dominik Heller <kilianmandscharo@gmail.com>
2022-03-19 15:53:59 +0100

    anchor tag warning fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
                     <a
                         href="https://github.com/kilianmandscharo"
                         target="_blank"
-                        rel="norefferer noopener"
+                        rel="norefferer"
                         className="sm:hover:text-gray-500 text-orange-300 transition-colors"
                     >
                         Dominik Heller
